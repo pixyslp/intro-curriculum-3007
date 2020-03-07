f9f6d0f8654d6852f446c9aa77813a446241c281
pixyslp <piko_ax@live.jp>
2020-03-08 02:40:28 +0900

    17で割り切れるか判定する関数を実装

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 'use strict';
-
-module.exports = {
+/**
+ * 17の倍数のときtrueを返す
+ * @param {number} number
+ */
+function isMultipleOfSeventeen(num){
+    return num % 17 === 0;
 }
+module.exports = {
+    isMultipleOfSeventeen
+};
